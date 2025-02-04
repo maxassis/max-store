4c95ad9156bdaf0ecbc17ff0ae21e6da08f80caa
max <max.assis@chatguru.com.br>
2025-02-04 11:55:05 -0300

    feat: iniciando o projeto

---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+export default function Card() {
+  return (
+    <div className="w-full h-80 bg-gray-200 rounded-md">
+      <div>
+        <img
+          src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
