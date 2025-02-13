8e10bb2a74936c098a43d1b5d22e037249c4725a
max <max.assis@chatguru.com.br>
2025-02-13 22:11:58 +0000

    13 de fev. de 2025, 19:11

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -15,3 +15,11 @@ export async function fetchProduct(id: string): Promise<Product> {
   }
   return response.json();
 }
+
+// export async function fetchData<T>(endpoint: string): Promise<T> {
+//   const response = await fetch(`http://localhost:3000/${endpoint}`);
+//   if (!response.ok) {
+//     throw new Error(`Erro ao buscar os dados: ${response.statusText}`);
+//   }
+//   return response.json();
+// }
