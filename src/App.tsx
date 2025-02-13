f333e77897a1b72b66c50b8f51aa73e9aa1d456b
max <max.assis@chatguru.com.br>
2025-02-13 18:31:07 -0300

    feat: feito pagina 404

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./Pages/Main";
 import Product from "./Pages/Product";
 import Sidebar from "./components/Sidebar";
+import NotFound from "./Pages/404";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/product/:id" element={<Product />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
